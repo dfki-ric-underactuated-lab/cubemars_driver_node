6afbe8e554e5071e1a044736b6419713533e111c
dmronga <dennis.mronga@dfki.de>
2026-08-07 17:16:07 +0200

    Check if save cmd works

--- src/mab_fd_can.cpp
+++ src/mab_fd_can.cpp
@@ -505,7 +505,12 @@ void MabFdCan::set_zero_position(unsigned int joint_id)
     send_zero_frame(joint_configs_[joint_id].can_id, zm);
 
     RunSaveCommand_Message sm;
-    send_register_command(joint_configs_[joint_id].can_id, &sm, sizeof(sm));
+    try {
+        send_register_command(joint_configs_[joint_id].can_id, &sm, sizeof(sm));
+    }catch (const can_device_error &e)
+    {
+        throw can_interface_error(std::format("Failed to save motor config {}", e.what()));
+    } 
 
     // Restore the configured receive timeout.
     tv.tv_sec = socket_timeout_sec_;
